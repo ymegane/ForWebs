7a5d14212828749abb50f25fd8c67408fdbcf873
ymegane <crybaby.sumire+github@gmail.com>
2019-06-13 22:51:34 +0900

    :+1: Use styled-components

--- expo/sampleapp/App.tsx
+++ expo/sampleapp/App.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, Button } from "react-native";
+import styled from "styled-components";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Container>
+      <Text>Hello World!</Text>
+      <Button title={"Button"} onPress={() => {}} />
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled(View)`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
